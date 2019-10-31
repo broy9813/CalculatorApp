7640bc2145ad87327c512457b1ed7399b0f16e2f
Bishakha Roy <seroy417@gmail.com>
2019-10-31 10:44:59 -0400

    First version of CalculatorApp.

--- CalculatorApp/ViewController.swift
+++ CalculatorApp/ViewController.swift
@@ -10,11 +10,65 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var number: UITextField!
+    
+    @IBOutlet weak var number2: UITextField!
+    
+    @IBOutlet weak var calculate: UILabel!
+    
+    var calculatedValue = ""
     override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+           super.viewDidLoad()
+        }
+    
+    
+    @IBAction func Addition(_ sender: UIButton) {
+        var dAdd = 0.0
     }
-
-
-}
+    
+    @IBAction func Subtraction(_ sender: UIButton) {
+         var dSubtract = 0.0
+    }
+    
+    @IBAction func Multiplictaion(_ sender: UIButton) {
+        var dMultiple = 0.0
+    }
+    
+    @IBAction func Divide(_ sender: UIButton) {
+          var dDivide = 0.0
+    }
+            
+            
+            var dDivide = 0.0
+           
+           // get the values in the TextFields, if a value exists and assign the values to constants
+           // the TextFields are Optionals, so they could contain a value and they could also be nil
+           if let value = numberTextField.text, let percent =
+               tipPercentTextField.text {
+               
+               // trim whitespace that may exist at the begining or the ending of the constants
+               let trimmedAmount = number.trimmingCharacters(in: .whitespaces)
+               let trimmedPercent = number2.trimmingCharacters(in: .whitespaces)
+               
+               // check to make sure the trimmed constants aren't empty
+               if (!trimmedAmount.isEmpty && !trimmedPercent.isEmpty){
+                   
+                   // Convert trimmed constants to Doubles
+                   dNumber = Double(trimmedAmount)!
+                   dNumber2 = Double(trimmedPercent)!
+                   
+                   // calculate the values
+                   dCalculate = dNumber + dNumber2
+                dCalculate = dNumber - dNumber2
+                dCalculate = dNumber * dNumber2
+                dCalculate = dNumber / dNumber2
+               }
+           }
+           
+           // convert calculated tip to a String
+           // displayed it in the Label
+           calculateValue.text = " is $\(String(dTip))"
+           
+          
+    }
 
